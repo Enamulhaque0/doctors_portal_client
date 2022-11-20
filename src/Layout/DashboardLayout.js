b2354c912a76ef8a9b8a3304e2d4851c7d98ca2d
Enamulhaque0e <enamulhaq243@gmail.com>
2022-11-20 11:00:17 +0600

    admin role

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email)
     return (
         <div>
             <Navbar></Navbar>
@@ -14,13 +18,12 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                        <h1>hello</h1>
-                        {/* <li><Link to="/dashboard">My Appointments</Link></li>
+                        <li><Link to="/dashboard">My Appointments</Link></li>
                         {
                             isAdmin && <>
                                 <li><Link to="/dashboard/allusers">All users</Link></li>
                             </>
-                        } */}
+                        }
 
                     </ul>
 
